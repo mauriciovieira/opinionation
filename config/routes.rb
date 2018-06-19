35b16c989c307d56a25c78b97535f3cc92055c15
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-06-19 09:21:24 +0300

    Add default route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :thoughts
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'thoughts#index'
 end
