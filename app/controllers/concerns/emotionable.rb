773c214d98003f31d1ac537d9ddd89533437ef8b
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-06-20 08:28:36 +0300

    Create emotionable concern

---
+++ app/controllers/concerns/emotionable.rb
@@ -0,0 +1,31 @@
+module Emotionable
+  extends ActiveSupport::Concern
+
+  included do
+    has_many :emotions, as: :emotionable
+  end
+
+  def add_emotion(emoji, current_user)
+    Emotion.create(emotionable: self, user: current_user, emoji: emoji)
+  end
+
+  def has_emotion?(curent_user)
+    set_emotion(current_user)
+    @emotion.present? ? @emotion : false
+  end
+
+  def update_emotion(emoji, current_user)
+    set_emotion(current_user)
+    @emotion.update(emoji: emoji)
+  end
+
+  def remove_emotion(current_user)
+    set_emotion(current_user)
+    @emotion.destroy
+  end
+
+  private
+  def set_emotion(current_user)
+    @emotion = Emotion.find_by(emotionable: self, user: current_user)
+  end
+end
