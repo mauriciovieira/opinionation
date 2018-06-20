5534ad09f03f2bfadc704ca7c969f0f60ee788b7
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-06-20 08:01:16 +0300

    Add create_observation route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :thoughts
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'thoughts#index'
+  post '/thought/:thougth_id/observation/', to: 'observations#create', as: 'create_observation'
 end
